5c93f87aa1d3c524c4dfbb3ad2ffc45525aa8025
ukly <dlwjddnr5438@kookmin.ac.kr>
2020-06-26 17:44:17 +0900

    issue #7 modfied template.py for formatting. %d -> %s to print string.

--- calc/template.py
+++ calc/template.py
@@ -2,12 +2,12 @@ html = """
 <html>
     <body>
         <form action="">
-                a:  <input type="number" name="a"><br><br>
-                b:  <input type="number" name="b">
+                a: <input type="number" name="a"><br><br>
+                b: <input type="number" name="b">
             <input type="submit">
         </form>
-        Sum = %(sum)d <br><br>
-        Prod =  %(prod)d
+        Sum = %(sum)s <br><br>
+        Prod = %(prod)s
     </body>
 </html>
 """
