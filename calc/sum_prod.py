ace3e9d957cbf333069466c47d915d4042e5fad3
ukly <dlwjddnr5438@kookmin.ac.kr>
2020-06-26 16:55:46 +0900

    issue #5 recent 3 commits are my mistake that misunderstood issue number so i modfied it. And about modfied sum_prod is define sum and prod equal 0 before 'if' and modfied sum1 and product1 in if sentence to sum and prod. And to formatting right, add 'sum':sum, 'prod':prod

--- calc/sum_prod.py
+++ calc/sum_prod.py
@@ -5,12 +5,12 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
-    #for modifed
+    sum, prod = 0, 0
     if '' not in [a, b]:
         a, b = int(a), int(b)
-        sum1 = a + b
-        product1 = a * b
-    response_body = html % {}
+        sum = a + b
+        product = a * b
+    response_body = html % {'sum':sum, 'prod':prod}
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
        ('Content-Length', str(len(response_body)))
