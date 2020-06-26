613c02437598acbf1ea9e5d070db368f9ebeba9e
ukly <dlwjddnr5438@kookmin.ac.kr>
2020-06-26 18:18:02 +0900

    issue #8 add error = 'No Error" and formatting error.

--- calc/sum_prod.py
+++ calc/sum_prod.py
@@ -5,6 +5,7 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
+    error = "No Error"
     sum, prod = "a와 b를 입력하면 합이 출력됩니다", "a와 b를 입력하면 곱이 출력됩니다"
     if '' not in [a, b]:
         sum, prod = 0, 0
@@ -12,7 +13,7 @@ def application(environ, start_response):
         sum = a + b
         prod = a * b
         sum, prod = str(sum), str(prod)
-    response_body = html % {'sum':sum, 'prod':prod}
+    response_body = html % {'sum':sum, 'prod':prod, 'error':error}
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
         ('Content-Length', str(len(response_body)))
