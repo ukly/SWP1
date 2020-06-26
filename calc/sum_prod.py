a791537c21ed765a73aa2d3db83316e737b7cc87
ukly <dlwjddnr5438@kookmin.ac.kr>
2020-06-26 16:51:20 +0900

    issue #4 modified it again for straighten mistake

--- calc/sum_prod.py
+++ calc/sum_prod.py
@@ -1,17 +1,16 @@
 from cgi import parse_qs
 from template import html
 
-#for modify mistkae 
 def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
-    sum, prod = 0, 0
+    #for modifed
     if '' not in [a, b]:
         a, b = int(a), int(b)
         sum1 = a + b
         product1 = a * b
-    response_body = html % {'sum':sum, 'prod':prod}
+    response_body = html % {}
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
        ('Content-Length', str(len(response_body)))
