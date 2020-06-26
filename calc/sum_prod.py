aa2b79711fe74e42cf5afb9a9209cdc758b3b410
ukly <dlwjddnr5438@kookmin.ac.kr>
2020-06-26 17:37:19 +0900

    issue #7 modifed sum and modfied to string sentences. And redefined in 'if sentence' equal 0. For print consistent format, formating sum and prod that calculating is done.

--- calc/sum_prod.py
+++ calc/sum_prod.py
@@ -5,11 +5,13 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
-    sum, prod = 0, 0
+    sum, prod = "a와 b를 입력하면 합이 출력됩니다", "a와 b를 입력하면 곱이 출력됩니다"
     if '' not in [a, b]:
+        sum, prod = 0, 0
         a, b = int(a), int(b)
         sum = a + b
         prod = a * b
+        sum, prod = str(sum), str(prod)
     response_body = html % {'sum':sum, 'prod':prod}
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
